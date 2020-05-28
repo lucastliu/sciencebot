f89bca11193697a196bb9c230878e0549461d4b6
Lucas Liu <ltl9@duke.edu>
2020-05-28 17:16:20 -0400

    main script and constants

---
+++ error_constants.py
@@ -0,0 +1,12 @@
+X_OFFSET_MAX = 80
+CM_PER_MOVE = 20
+
+
+ACCEPTABLE_TURN_ERROR_DEGREES = 2
+ACCEPTABLE_TURN_ERROR = math.radians(ACCEPTABLE_TURN_ERROR_DEGREES)
+MAX_NUM_TURN_ADJUSTMENTS = 20
+
+
+fov_degrees = 70
+fov_rad = math.radians(fov_degrees)
+search_turn_mag_degrees = fov_degrees/2
