aa505ff679ab6384e1c2311721980c92b122f1c5
Lucas <lucas.liu@duke.edu>
2020-07-02 12:40:26 -0500

    improved imu api and library

--- dev_ws/src/nav/nav/imu/imu.py
+++ dev_ws/src/nav/nav/imu/imu.py
@@ -4,7 +4,10 @@ from rclpy.node import Node
 from std_msgs.msg import String
 from turtlesim.msg import Pose
 
-from nav.imu.imu_integrated_movement import *
+import time
+import board
+import busio
+import adafruit_bno055
 
 
 class IMU(Node):
@@ -12,15 +15,45 @@ class IMU(Node):
     def __init__(self):
         super().__init__('imu')
         self.publisher_ = self.create_publisher(Pose, 'heading', 10)     # CHANGE
-        timer_period = 1.0  # seconds
+        timer_period = .1  # seconds
         self.timer = self.create_timer(timer_period, self.timer_callback)
-        self.i = 0
+        
+        self.pos_pub = self.create_publisher(Pose, 'imu_position', 10)     # CHANGE
+
+        # Use these lines for I2C
+        i2c = busio.I2C(board.SCL, board.SDA)
+        self.sensor = adafruit_bno055.BNO055_I2C(i2c)
+        
+        self.x = 0.0
+        self.y = 0.0
+        self.theta = 0.0
+        self.accel = 0.5 * timer_period * timer_period # p = .5at^2
+
+        # User these lines for UART
+        # uart = busio.UART(board.TX, board.RX)
+        # sensor = adafruit_bno055.BNO055_UART(uart)
 
     def timer_callback(self):
         msg = Pose()
-        msg.theta = getYaw("/home/pi/pi-bno055/getbno055")
+        self.position_update()
+        msg.x = self.x
+        msg.y = self.y
+        msg.theta = self.theta
         self.publisher_.publish(msg)
-        self.get_logger().info('Heading: "%s"' % msg.theta)
+        self.get_logger().info('IMU Pose: "%s"' % msg)
+        
+    def position_update(self):
+            x = self.sensor.linear_acceleration[0]
+            y = self.sensor.linear_acceleration[1]
+            theta = self.sensor.euler[0]
+            
+            if x:
+                self.x = x
+            if y:
+                self.y = y
+            if theta:
+                self.theta = theta
+
 
 
 def main(args=None):
