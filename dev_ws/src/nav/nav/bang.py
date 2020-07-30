4969523d4ae2bd88a4b33949c03df2a01a82ec9c
Lucas <lucas.liu@duke.edu>
2020-07-30 09:44:42 -0500

    smoother bb movement

--- dev_ws/src/nav/nav/bang.py
+++ dev_ws/src/nav/nav/bang.py
@@ -37,7 +37,6 @@ class Bang(Node):
             callback_group=self.group)
 
         self.sm = SerialMotor("/dev/ttyACM1")
-        self.get_logger().info('Motors Node Live')
         
 
         self.x_dest = 0.0
@@ -47,10 +46,10 @@ class Bang(Node):
         self.angle = 0.0
         self.r = 1.0
         self.angle_diff = 10
-
+        self.old = [0.0, 0.0]
         self.twist = Twist()
         
-        self.get_logger().info('PID Node Live')
+        self.get_logger().info('BB Node Live')
     def pose_callback(self, pose):
         #self.get_logger().info('Pose: %s' % (pose))  # CHANGE
         self.x = pose.x
@@ -70,15 +69,23 @@ class Bang(Node):
         self.y_dest = goal_handle.request.y_dest
         self.r = self.get_distance()
         self.get_logger().info('Start r: {0}'.format(self.r))
+        self.old = [0, 0]
 
         # return x,y during each cycle
         # return final position
         feedback_msg = Tune.Feedback()
 
-        while self.r > 0.4:
+        while self.r > 0.2:
 
             # calculate
+            self.angle_diff = self.angle - self.steering_angle()
+            
+            if self.angle_diff > math.pi:
+                self.angle_diff = self.angle_diff - 2*math.pi
 
+            if self.angle_diff < -1*math.pi:
+                self.angle_diff = self.angle_diff + 2*math.pi
+                
             self.angular_correction()
             self.linear_correction()
             
@@ -102,7 +109,7 @@ class Bang(Node):
         result.x_final = self.x
         result.y_final = self.y
         
-
+        self.angle_diff = 10
         
         self.get_logger().info('Finish Move To')
 
@@ -115,17 +122,36 @@ class Bang(Node):
             + math.pow(self.y_dest - self.y, 2)
             )
 
+    def linear_smooth(self, d):
+        if d > 1:
+            return 1
+        elif d < .6:
+            return .6
+        else:
+            return d
+    
+    def angular_smooth(self, a):
+        return .7
+        #y = a / math.pi
+        #if y < .5:
+        #    y = .5
+        #return y
+            
     def linear_correction(self):
         self.r = self.get_distance()
-        self.sm.set_motor(3, .6)  # right
-        self.sm.set_motor(4, .6)  # left
-        L = self.r
+        speed = -1*self.linear_smooth(self.r)
+        #print("Speed: {}".format(speed))
+        L = .2*self.old[0] + .8*speed
+        R = .2*self.old[1] + .8*speed
+        self.sm.set_motor(4, L)  # left
+        self.sm.set_motor(3, R)  # right
+        
+        L = self.r/4
         time.sleep(L)
-        
-        self.sm.set_motor(3, 0)  # right
-        self.sm.set_motor(4, 0)  # left
-        
-        
+        self.old = [L, R]
+        #self.sm.set_motor(3, 0)  # right
+        #self.sm.set_motor(4, 0)  # left
+        #time.sleep(0.1)
 
     def angular_correction(self):
         while abs(self.angle_diff) > math.pi / 45:
@@ -136,16 +162,27 @@ class Bang(Node):
 
             if self.angle_diff < -1*math.pi:
                 self.angle_diff = self.angle_diff + 2*math.pi
-            
-            if self.angle_diff > 0:
-                self.sm.set_motor(3, -.5)  # right
-                self.sm.set_motor(4,.5)  # left
-            else:
-                self.sm.set_motor(3, .5)  # right
-                self.sm.set_motor(4, -.5)  # left
                 
-        self.sm.set_motor(3, 0)  # right
-        self.sm.set_motor(4, 0)  # left
+            spin = self.angular_smooth(abs(self.angle_diff))
+            #print("Spin: {}".format(spin))
+            if self.angle_diff > 0:
+                L = spin
+                R = -1*spin
+
+            else:
+                L = -1*spin
+                R = spin
+            
+            L = .2*self.old[0] + .8*L
+            R = .2*self.old[1] + .8*R
+            self.sm.set_motor(4, L)
+            self.sm.set_motor(3, R)
+            
+            self.old = [L, R]
+                
+        #self.sm.set_motor(3, 0)  # right
+        #self.sm.set_motor(4, 0)  # left
+        #time.sleep(.1)
         
     def steering_angle(self):
         return math.atan2(self.y_dest - self.y, self.x_dest - self.x)
