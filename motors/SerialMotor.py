49dbdae6d906c45ad6c494f57b5df11227a8fcd6
Lucas <lucas.liu@duke.edu>
2020-08-03 09:39:05 -0500

    motor interface change

--- motors/SerialMotor.py
+++ motors/SerialMotor.py
@@ -7,4 +7,4 @@ class SerialMotor:
 
     def set_motor(self, motor_port, power):
         """Sends a message to this serial port of the form "z<motor_port> <power>\n", with power to two decimal places."""
-        self.port.write(("z%d %.2f\n" % (motor_port, power)).encode("utf-8"))
+        self.port.write(("z%d %.2f\n" % (motor_port, -1*power)).encode("utf-8"))
