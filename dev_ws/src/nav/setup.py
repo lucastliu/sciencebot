c2c33c674232a29eff651c88122264b5d200d652
Lucas <lucas.liu@duke.edu>
2020-08-10 08:35:19 -0500

    nav package versioning

--- dev_ws/src/nav/setup.py
+++ dev_ws/src/nav/setup.py
@@ -8,7 +8,7 @@ package_name = 'nav'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='1.0.0',
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
